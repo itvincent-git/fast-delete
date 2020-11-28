0a62ad05bcf4763beab2a9661934bad3e50095d1
梅花扣肉 <xul0038@qq.com>
2020-11-28 09:29:46 +0800

    修复路径包含空格的问题

--- generate-reg.js
+++ generate-reg.js
@@ -3,15 +3,16 @@ const fs = require('fs-extra')
 var execa = require('execa');
 
 var path = __dirname
+console.log("current path = " + path)
 const regFile = "autogen-reg.reg"
-var path = path.replace("\\", "\\\\\\\\")
+var path = path.replace(/\\/g, "\/")
 console.log("current path = " + path)
 
 var data = "Windows Registry Editor Version 5.00\n\n" +
 "[HKEY_CLASSES_ROOT\\Folder\\shell\\fastdelete]\n" +
 "@=\"极速删除\"\n\n"+
 "[HKEY_CLASSES_ROOT\\Folder\\shell\\fastdelete\\command]\n" +
-"@=\"cmd.exe /k node " + path + "\\\\\\\\delete.js %V\"\n";
+"@=\"cmd.exe /k node " + path + "/delete.js \\\"%V\\\"\"\n";
 
 fs.writeFile(regFile, data, function(err){
     if (err) {
